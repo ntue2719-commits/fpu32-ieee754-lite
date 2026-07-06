680ead41935e3fc1f9e899d66abfa064a009e73d
Tri <ntue2719@gmail.com>
2026-07-06 19:30:41 +0700

    feat: add fp_add_sub_core for IEEE-754 mantissa arithmetic

---
+++ src/adder_subtractor/fp_add_sub_core.v
@@ -0,0 +1,18 @@
+module add_sub (
+    input A_sign,
+    input B_sign,
+    input [23:0] A_mantissa,
+    input [23:0] B_mantissa,
+    output reg [24:0] sum_mantissa
+);
+    always @(*) begin
+        if(A_sign == B_sign) begin
+            // Same sign -> add aligned magnitudes
+             sum_mantissa = {1'b0, A_mantissa} + {1'b0, B_mantissa}; 
+        end
+        else begin
+            // Assume |A| >= |B| (guaranteed by fp_align)
+            sum_mantissa = {1'b0, A_mantissa} - {1'b0, B_mantissa}; 
+        end      
+    end
+endmodule
