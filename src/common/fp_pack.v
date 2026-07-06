277c1211a94e5c76eea1075eeed8188842c72c09
Tri <ntue2719@gmail.com>
2026-07-07 02:16:34 +0700

    feat(common): add fp_pack.v module for IEEE-754

---
+++ src/common/fp_pack.v
@@ -0,0 +1,11 @@
+module fp_pack(
+    input        sign,
+    input  [7:0] exponent,
+    input  [22:0] fraction,
+
+    output [31:0] result
+);
+
+assign result = {sign, exponent, fraction};
+
+endmodule
