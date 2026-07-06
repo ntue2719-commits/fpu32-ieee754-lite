ff2897f6af7a3203d5cd484ec5765bcad02e4e5b
Tri <ntue2719@gmail.com>
2026-07-07 02:09:28 +0700

    refactor(adder): use 9-bit internal exponent in fp_align

--- src/adder_subtractor/fp_align.v
+++ src/adder_subtractor/fp_align.v
@@ -8,13 +8,13 @@
 //   - Generate Guard, Round and Sticky bits
 //==============================================================================
 
-module align (
+module fp_align (
     input  [31:0] A,
     input  [31:0] B,
 
     output        A_sign,
     output        B_sign,
-    output [7:0]  base_E,
+    output [7:0]  exponent,
     output [23:0] A_mantissa_ext,
     output [23:0] B_mantissa_shifted,
 
@@ -41,7 +41,7 @@ wire [31:0] min_val = comp ? B : A;
 // Extract sign and use the larger exponent as the base exponent
 assign A_sign = max_val[31];
 assign B_sign = min_val[31];
-assign base_E = max_val[30:23];
+assign exponent = max_val[30:23];
 
 
 // Restore the hidden bit
